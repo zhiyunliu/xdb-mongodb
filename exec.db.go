6caec62be94ccb9a1d0723a0b50a5c236b7abeac
liujinyin <liujinyin@xunyou.com>
2025-02-13 16:15:59 +0800

    适配xdb-mongodb到 到glue v0.7版本

--- exec.db.go
+++ exec.db.go
@@ -8,42 +8,44 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var _ xdb.IDB = (*mongodb)(nil)
+
 type mongodb struct {
 	cfg      *contribxdb.Setting
 	client   *mongo.Client
 	database *mongo.Database
 }
 
-func (db *mongodb) Query(ctx context.Context, sql string, input any) (data xdb.Rows, err error) {
+func (db *mongodb) Query(ctx context.Context, sql string, input any, opts ...xdb.TemplateOption) (data xdb.Rows, err error) {
 	err = NotImplemented
 	return
 }
 
-func (db *mongodb) Multi(ctx context.Context, sql string, input any) (data []xdb.Rows, err error) {
+func (db *mongodb) Multi(ctx context.Context, sql string, input any, opts ...xdb.TemplateOption) (data []xdb.Rows, err error) {
 	err = NotImplemented
 	return
 }
 
-func (db *mongodb) First(ctx context.Context, sql string, input any) (data xdb.Row, err error) {
+func (db *mongodb) First(ctx context.Context, sql string, input any, opts ...xdb.TemplateOption) (data xdb.Row, err error) {
 	err = NotImplemented
 	return
 }
 
-func (db *mongodb) Scalar(ctx context.Context, sql string, input any) (data interface{}, err error) {
+func (db *mongodb) Scalar(ctx context.Context, sql string, input any, opts ...xdb.TemplateOption) (data interface{}, err error) {
 	err = NotImplemented
 	return
 }
 
-func (db *mongodb) Exec(ctx context.Context, sql string, input any) (r xdb.Result, err error) {
+func (db *mongodb) Exec(ctx context.Context, sql string, input any, opts ...xdb.TemplateOption) (r xdb.Result, err error) {
 	err = NotImplemented
 	return
 }
 
-func (db *mongodb) QueryAs(ctx context.Context, sql string, input any, result any) (err error) {
+func (db *mongodb) QueryAs(ctx context.Context, sql string, input any, result any, opts ...xdb.TemplateOption) (err error) {
 	return NotImplemented
 }
 
-func (db *mongodb) FirstAs(ctx context.Context, sql string, input any, result any) (err error) {
+func (db *mongodb) FirstAs(ctx context.Context, sql string, input any, result any, opts ...xdb.TemplateOption) (err error) {
 	return NotImplemented
 }
 
