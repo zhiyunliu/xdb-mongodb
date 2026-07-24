ba5aeb35b859fcf3fc9c065dc4aa3609ce0eb4c2
liujinyin <liujinyin@xunyou.com>
2026-07-25 00:35:56 +0800

    修改metrics的名称

--- metrics.go
+++ metrics.go
@@ -16,7 +16,7 @@ var (
 )
 
 type Metrics struct {
-	RequestCounter metrics.Int64UpDownCounter `metric:"cur_proc"  `
+	RequestCounter metrics.Int64UpDownCounter `metric:"db_cur_proc"  `
 }
 
 func InitMetrics() (err error) {
@@ -32,12 +32,12 @@ func (m *Metrics) Incr(connName, spanName string) {
 	if m.RequestCounter == nil {
 		return
 	}
-	m.RequestCounter.Add(context.Background(), 1, metric.WithAttributes(attribute.String("conn", connName), attribute.String("span", spanName)))
+	m.RequestCounter.Add(context.Background(), 1, metric.WithAttributes(attribute.String("dbtype", "mongo"), attribute.String("conn", connName), attribute.String("span", spanName)))
 }
 
 func (m *Metrics) Decr(connName, spanName string) {
 	if m.RequestCounter == nil {
 		return
 	}
-	m.RequestCounter.Add(context.Background(), -1, metric.WithAttributes(attribute.String("conn", connName), attribute.String("span", spanName)))
+	m.RequestCounter.Add(context.Background(), -1, metric.WithAttributes(attribute.String("dbtype", "mongo"), attribute.String("conn", connName), attribute.String("span", spanName)))
 }
